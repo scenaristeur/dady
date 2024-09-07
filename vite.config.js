a9731011a7780b10ec9375d18bd5f6a9fd5a98ea
scenaristeur <scenaristeur@gmail.com>
2024-09-07 13:44:05 +0200

    env.MODE

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-const env = loadEnv('all', process.cwd())
-const base = env.NODE_ENV === 'production' ? '/dady/' : '/'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: base,
+  base: import.meta.env.MODE === 'production' ? '/dady/' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
