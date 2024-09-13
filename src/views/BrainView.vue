4e57e3e5b648c7eb2efa0f46829e7deaa2891af1
David Faveris <david.faveris@dgfip.finances.gouv.fr>
2024-09-13 16:35:23 +0200

    edit

--- src/views/BrainView.vue
+++ src/views/BrainView.vue
@@ -1,44 +1,70 @@
 <template>
   <div>
     BrainView
-    <button @click="newNode">New Brain/Node</button> {{ status }}
+    <button @click="newNode" class="btn btn-primary">New Brain/Node</button> {{ status }}
 
     <!-- Button trigger modal -->
 
     <div v-if="node">
-      {{ node }}
+      <!-- {{ node }} -->
 
-      <b-button variant="success" class="m-3"
+      <button class="btn btn-primary" disabled
         @click="node['ve:type'] = node['ve:type'] == undefined || node['ve:type'] == 'html' ? 'node' : 'html'">
         {{ node['ve:type'] == 'node' ? "Switch to Html Edition" : "Switch to Node Edition" }}
 
 
-      </b-button>
+      </button>
+
+
+
+      <div v-if="node['ve:type'] == 'node'">
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="text" v-model="node['ve:name']" autocomplete="off" autofocus class="form-control" id="name"
+            aria-describedby="nameHelp" placeholder="Enter Node Name">
+          <small id="nameHelp" class="form-text text-muted">Provide a Nice name to your node.</small>
+        </div>
+
+        <div class="form-group">
+          <label for="age">Age</label>
+          <input v-model="node['ve:age']" required type="number"  class="form-control" id="age" placeholder="Age">
+        </div>
+
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+
+        <br>
+        <button class="btn btn-primary" @click="saveNode">Save Node</button>
+      </div>
+
+
       <b-container v-if="node['ve:type'] == 'node'">
 
-        <b-row>
-          <b-col sm="3">
+        <!-- <div>
+          <div sm="3">
             <label for="name">Name:</label>
-          </b-col>
-          <b-col sm="9">
-            <b-form-input id="name" v-model="node['ve:name']" autocomplete="off" autofocus />
-          </b-col>
-          <b-col sm="3">
+          </div>
+          <div sm="9">
+            <input id="name" v-model="node['ve:name']" autocomplete="off" autofocus />
+          </div>
+          <div sm="3">
             <label for="age"><code>Age</code>:</label>
-          </b-col>
-          <b-col>
-            <b-form-input id="age" v-model="node['ve:age']" required type="number" />
-          </b-col>
-        </b-row>
+          </div>
+          <div>
+            <input id="age" v-model="node['ve:age']" required type="number" />
+          </div>
+        </div> -->
 
 
-        <b-row v-for="p in node['ve:properties']" :key="p.name">
-          <b-col sm="3">
+        <div v-for="p in node['ve:properties']" :key="p.name">
+          <div sm="3">
             <label for="name"><code>{{ p.name }}</code>:</label>
-          </b-col>
-          <b-col sm="9">
+          </div>
+          <div sm="9">
 
-            <!-- <b-button-toolbar key-nav aria-label="Toolbar with button groups">
+            <!-- <button-toolbar key-nav aria-label="Toolbar with button groups">
           <b-dropdown size="sm" class="mx-1" right text="+" variant="outline-primary">
           <b-dropdown-item @click="fieldType = 'text'">Text</b-dropdown-item>
           <b-dropdown-item @click="fieldType = 'textarea'">Textarea</b-dropdown-item>
@@ -46,48 +72,48 @@
           <b-dropdown-item @click="fieldType = 'link'">Link</b-dropdown-item>
         </b-dropdown>
 
-      </b-button-toolbar> -->
-            <b-button @click="showFieldModal(p)" variant="outline-primary">+</b-button>
+      </button-toolbar> -->
+            <button @click="showFieldModal(p)" variant="outline-primary">+</button>
             <!-- {{ p.values}} -->
 
             <ValuesComponent :values="p.values" />
 
-            <!-- <b-form-input id="name" v-model="node.name" required /> -->
-          </b-col>
-        </b-row>
+            <!-- <input id="name" v-model="node.name" required /> -->
+          </div>
+        </div>
 
-        <b-row v-if="field != null" class="mt-3">
-          <b-col sm="3">
-            <b-form-input @change="fieldNameChanged" v-model="field.name" required placeholder="property name" />
-          </b-col>
-          <b-col sm="9">
-            <b-button variant="outline-secondary" size="sm" @click="clear_field">X</b-button>
-          </b-col>
-        </b-row>
+        <div v-if="field != null" class="mt-3">
+          <div sm="3">
+            <input @change="fieldNameChanged" v-model="field.name" required placeholder="property name" />
+          </div>
+          <div sm="9">
+            <button variant="outline-secondary" size="sm" @click="clear_field">X</button>
+          </div>
+        </div>
 
-        <b-row>
-          <b-col>
-          </b-col>
-          <b-col>
-            <b-btn variant="outline-primary" size="sm" @click="add">+ add a property or a link</b-btn>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col>
-            <!-- <b-button  :variant="node['ve:privacy'] == 'public' ? 'warning' : 'outline-success'"
+        <div>
+          <div>
+          </div>
+          <div>
+            <button variant="outline-primary" size="sm" @click="add">+ add a property or a link</button>
+          </div>
+        </div>
+        <div>
+          <div>
+            <!-- <button  :variant="node['ve:privacy'] == 'public' ? 'warning' : 'outline-success'"
       @click="node['ve:privacy'] = node['ve:privacy'] == undefined || node['ve:privacy'] == 'public' ? 'private' : 'public'">
-      {{node['ve:privacy']}}</b-button> -->
+      {{node['ve:privacy']}}</button> -->
 
-            <b-btn variant="success" @click="saveNode">Save Node</b-btn>
+            <button variant="success" @click="saveNode">Save Node</button>
             <PermissionsLayout :permissions="permissions" :url="node['ve:url']" :autorized="autorized" />
-          </b-col>
-        </b-row>
+          </div>
+        </div>
 
         <b-modal id="fieldModal" size="xl" :title="node['ve:name'] + ' -> ' + currentProp.name">
           <!-- {{ currentProp}} -->
           <b-tabs content-class="mt-3">
             <b-tab title="text" active @click="fieldType = 'text'">
-              <b-form-input v-model="newvalue" placeholder="new value" @change="addNewValue" />
+              <input v-model="newvalue" placeholder="new value" @change="addNewValue" />
             </b-tab>
             <b-tab title="textarea" @click="fieldType = 'textarea'">
               <b-form-textarea v-model="newvalue" placeholder="Enter something..." rows="3" max-rows="6"
@@ -97,8 +123,8 @@
               <NodeSelector :currentProp.sync="currentProp" />
             </b-tab>
             <b-tab title="link" @click="fieldType = 'link'">
-              <b-form-input v-model="link.name" placeholder="name" />
-              <b-form-input v-model="link.href" placeholder="link" @change="addNewLink" />
+              <input v-model="link.name" placeholder="name" />
+              <input v-model="link.href" placeholder="link" @change="addNewLink" />
             </b-tab>
             <!-- <b-tab title="tiny" @click="fieldType = 'tiny'">
 
@@ -136,7 +162,7 @@
 
 <script>
 
-import CKWysiwyg from './CKWysiwyg.vue'; 
+import CKWysiwyg from './CKWysiwyg.vue';
 import NodeSelector from '../components/NodeSelector.vue'
 import ValuesComponent from '../components/ValuesComponent.vue'
 import PermissionsLayout from '../components/layout/PermissionsLayout.vue';
@@ -155,8 +181,8 @@ export default {
   data() {
     return {
       status: null,
-      node:null,
-      field : null,
+      node: null,
+      field: null,
       clearing: false,
       fieldType: "text",
       show: false,
@@ -169,30 +195,64 @@ export default {
     }
 
   },
-// created(){
-// this.node = this.$store.state.nodes.currentNode || this.$store.state.nodes.modele
-// },
+  // created(){
+  // this.node = this.$store.state.nodes.currentNode || this.$store.state.nodes.modele
+  // },
   methods: {
     newNode() {
       // this.$store.commit('nodes/newNode', this.modele)
       this.node = this.$store.state.nodes.currentNode || this.$store.state.nodes.modele
     },
-    async saveNode() {
-
-      this.status = await this.$store.dispatch('nodes/saveNode', this.node);
+    async save() {
+      console.log("saving",Object.assign({}, this.node))
+      await this.$store.dispatch('nodes/saveNode', this.node);
       this.$store.commit('nodes/setCurrentNode', null)
       this.$router.push('/');
     },
-    async getPermissions() {
+    add(){
+      this.field = {name: ""}
+    },
+    fieldNameChanged(field_name){
+      console.log(field_name)
+      if(this.clearing == false){
+        let p = {name: field_name, values: []}
+        this.node['ve:properties'] == undefined ? this.node['ve:properties'] = [] : ""
+        var index = this.node['ve:properties'].findIndex(x => x.name==p.name);
+        index === -1 ? this.node['ve:properties'].push(p) : alert(p.name+" already exist")
+      }
+    },
+    clear_field(){
+      this.clearing = true
+      this.field = null
+      this.clearing = false
+    },
+    showFieldModal(p){
+      console.log(p)
+      this.fieldType= "text"
+      this.currentProp = p
+      this.$bvModal.show("fieldModal")
+    },
+    addNewValue(){
+      let val = {value: this.newvalue,  type: this.fieldType}
+      this.currentProp.values.push(val)
+      this.newvalue = null
+    },
+    addNewLink(){
+      //console.log(this.link)
+      let val = {value: this.link,  type: this.fieldType}
+      this.currentProp.values.push(val)
+      this.link = {}
+    },
+    async getPermissions(){
       this.permissions = this.node['ve:url'] != undefined ? await this.$getPermissions(this.node) : null
       console.log("PERMISSIONS", this.permissions)
     }
   },
   currentNode() {
-      console.log("CURRENT NODE Changed")
-      this.node = this.currentNode
-      this.getPermissions()
-    },
+    console.log("CURRENT NODE Changed")
+    this.node = this.currentNode
+    this.getPermissions()
+  },
   computed: {
     currentNode() {
       return this.$store.state.nodes.currentNode;
